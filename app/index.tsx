6b1812c77c3e40833334a1309ee2438b13b58443
Bahaa Youssof <kjmustafa@alinma.com>
2025-06-26 09:18:35 +0300

    initial setup

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button, Heading, View } from "tamagui";
+
+export default function Home() {
+  return (
+    <View flex={1} alignItems="center" justifyContent="center">
+      <Heading size={"$9"} color={"gray"}>
+        Health Care
+      </Heading>
+    </View>
+  );
+}
